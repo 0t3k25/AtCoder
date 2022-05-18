ad5e776fc45a67d2bc01a750775f9e0470fe8b7e
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2022-05-18 16:21:43 +0000

    B answer

--- B/251.py
+++ B/251.py
@@ -1,30 +1,21 @@
 N, W = map(int, input().split())
 A = list(map(int, input().split()))
-arr_n = A
-count = 0
-i = 0
-j = 1
-k = 2
-while i < N - 1:
-    arr_n.append(A[i] + A[j])
-    j += 1
-    if j > N - 1:
-        i += 1
-        j = i + 1
-i = 0
-j = 1
-k = 2
-while i < N - 2:
-    arr_n.append(A[i] + A[j] + A[k])
-    j += 1
-    k += 1
-    if k > N - 1:
-        i += 1
-        j = i + 1
-        k = j + 1
 
-arr_n = list(set(arr_n))
-for i in arr_n:
-    if W >= i:
-        count += 1
-print(count)
+count = [0] * (W + 1)
+
+for i in A:
+    if i <= W:
+        count[i] = 1
+
+for i in range(N):
+    for j in range(i + 1, N):
+        if A[i] + A[j] <= W:
+            count[A[i] + A[j]] = 1
+
+for i in range(N):
+    for j in range(i + 1, N):
+        for k in range(j + 1, N):
+            if A[i] + A[j] + A[k] <= W:
+                count[A[i] + A[j] + A[k]] = 1
+
+print(sum(count))
