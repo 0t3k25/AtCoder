8ceb1bcc63e4528f9e2036db06d51d74cbf50ebe
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2022-05-21 16:49:23 +0000

    see answer

--- A/250.py
+++ A/250.py
@@ -1,27 +1,9 @@
-H, W = (int(x) for x in input().split())
-R, C = (int(x) for x in input().split())
-
-
-next_count = 0
-# 上
-up_R = R - 1
-if 1 <= up_R < H:
-    next_count += 1
-
-# 下
-down_R = R + 1
-if 1 < down_R <= H:
-    next_count += 1
-
-# 左
-left_C = C - 1
-if 1 <= left_C < W:
-    next_count += 1
-
-
-right_C = C + 1
-if 1 < right_C <= W:
-    next_count += 1
-
-
-print(next_count)
+if __name__ == "__main__":
+    H, W = (int(x) for x in input().split())
+    C, D = (int(x) for x in input().split())
+    count = 0
+    for a in range(1, H + 1):
+        for b in range(1, W + 1):
+            if abs(a - C) + abs(b - D) == 1:
+                count += 1
+    print(count)
