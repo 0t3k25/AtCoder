22a5177b8424266b2b166189f4502d40f1df2dd5
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2022-05-25 15:44:36 +0000

    my answer

---
+++ leetcode/del/Remove_Element.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+nums = [1, 2, 1, 4]
+val = 1
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = len(nums) - 1
+        while i >= 0:
+            print("nums:{}".format(nums[i]))
+            if nums[i] == val:
+                del nums[i]
+            i -= 1
+        print(nums)
+
+
+a = Solution()
+a.removeElement(nums, val)
