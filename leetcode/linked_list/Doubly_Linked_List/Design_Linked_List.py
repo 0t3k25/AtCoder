3126459b35fbe55bb08c7f21c04fb2c51ba62661
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-11-09 23:19:00 +0900

    WIP:Doubly Linked List

---
+++ leetcode/linked_list/Doubly_Linked_List/Design_Linked_List.py
@@ -0,0 +1,98 @@
+class MyLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def get(self, index: int) -> int:
+        val = self.head
+        while index > 0 and val is not None:
+            val = val.next
+            index -= 1
+        if val is None:
+            return -1
+        return val.value
+
+    def addAtHead(self, val: int) -> None:
+        new_node = Node(val, None, self.head)
+        if self.head:
+            self.head.prev = new_node
+        else:
+            
+
+
+    def addAtTail(self, val: int) -> None:
+        point = self.head
+        # 空リスト
+        if point is None:
+            self.addAtHead(val)
+            return
+
+        # 最後のノードまで
+        while point.next is not None:
+            point = point.next
+        point.next = Node(val)
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        # indexが0なら先頭に追加
+        if index == 0:
+            self.addAtHead(val)
+            return
+
+        point = self.head
+        current_index = 0
+
+        # 指定されたindexの一つ前の位置まで移動
+        while current_index < index - 1 and point is not None:
+            point = point.next
+            current_index += 1
+
+        # indexがリストの長さより大きい場合は挿入せず終了
+        if point is None:
+            return
+
+        # 末尾に追加する場合
+        if point.next is None and current_index + 1 == index:
+            self.addAtTail(val)
+            return
+
+        # 通常の挿入処理
+        point.next = Node(val, point.next)
+
+    def deleteAtIndex(self, index: int) -> None:
+        # indexが0なら先頭に追加
+        if index == 0:
+            if self.head is not None:  # リストが空でないことを確認
+                self.head = self.head.next
+            return
+
+        point = self.head
+        current_index = 0
+
+        # 指定されたindexの一つ前の位置まで移動
+        while current_index < index - 1 and point is not None:
+            point = point.next
+            current_index += 1
+
+        if point is None or point.next is None:
+            return
+
+        # 通常の削除処理
+        point.next = point.next.next
+
+
+class Node:
+
+    def __init__(self, val: int, prev=None, next=None):
+        self.value = val
+        self.prev = prev
+        self.next = next
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
