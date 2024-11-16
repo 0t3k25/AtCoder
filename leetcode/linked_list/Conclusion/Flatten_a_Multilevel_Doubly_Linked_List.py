eb1aef2e39c96f83bde176e4946d68ec4373e2c1
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-11-16 20:03:14 +0900

    not using recursion

--- leetcode/linked_list/Conclusion/Flatten_a_Multilevel_Doubly_Linked_List.py
+++ leetcode/linked_list/Conclusion/Flatten_a_Multilevel_Doubly_Linked_List.py
@@ -1,21 +1,20 @@
-
 class Solution:
     def flatten(self, head: "Optional[Node]") -> "Optional[Node]":
-# 
-#         if not head:
-#             return head
-#         cur = head
-#         while cur:
-#             if cur.child:
-#                 child_first = cur.child
-#                 child = cur.child
-#                 while child.next:
-#                     child = child.next
-#                 child.next = cur.next
-#                 if child.next:
-#                     child.next.prev = child
-#                 cur.next = child_first
-#                 cur.child =None
-#                 child_first.prev = cur
-#             cur = cur.next
-#         return head
+
+        if not head:
+            return head
+        cur = head
+        while cur:
+            if cur.child:
+                child_first = cur.child
+                child = cur.child
+                while child.next:
+                    child = child.next
+                child.next = cur.next
+                if child.next:
+                    child.next.prev = child
+                cur.next = child_first
+                cur.child = None
+                child_first.prev = cur
+            cur = cur.next
+        return head
