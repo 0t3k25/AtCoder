b781cbb084a175882dfaeb8b49e02d4301f33537
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-11-20 01:43:52 +0900

    finish rotate Linked List

--- leetcode/linked_list/Conclusion/Rotate_List.py
+++ leetcode/linked_list/Conclusion/Rotate_List.py
@@ -7,3 +7,29 @@ class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if not head:
             return None
+        cur = head
+        list_size = 0
+
+        # mesure list size
+        while cur:
+            list_size += 1
+            cur = cur.next
+
+        # calculate rotate count
+        rotate_count = k % list_size
+
+        last_num = list_size - rotate_count
+        new_head = head
+        if last_num != list_size:
+            connect_list = head
+            while last_num > 1:
+                connect_list = connect_list.next  # 新しいヘッドの終点
+                last_num -= 1
+            new_head = connect_list.next
+            connect_list.next = None
+            cur = new_head
+            while cur.next:
+                cur = cur.next
+            cur.next = head
+
+        return new_head
