3594d6ee6f188e71b1222f1fcf335c55e23cf265
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-11-25 11:09:27 +0900

    level order traversal

---
+++ leetcode/Binary_Tree/Conclusion/Populating_Next_Right_Pointers_in_Each_Node.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+
+class Solution:
+    def connect(self, root: "Optional[Node]") -> "Optional[Node]":
+        if not root:
+            return None
+        queue = deque([root])
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            for i in range(len(level) - 1):
+                level[i].next = level[i + 1]
+
+        return root
