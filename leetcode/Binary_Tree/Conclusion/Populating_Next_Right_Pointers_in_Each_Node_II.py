b3088a03fff5a5e3bb435978e994b9872fc7e9b7
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-12-01 03:03:14 +0900

    DFS algorithms

--- leetcode/Binary_Tree/Conclusion/Populating_Next_Right_Pointers_in_Each_Node_II.py
+++ leetcode/Binary_Tree/Conclusion/Populating_Next_Right_Pointers_in_Each_Node_II.py
@@ -1,34 +1,31 @@
-from collections import deque
-
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.next = next
-"""
-
-
 class Solution:
     def connect(self, root: "Node") -> "Node":
-        if not root:
-            return root
+        def helper(node: "Node"):
+            if not node:
+                return None
 
-        queue = deque([root])
+            scanner = node.next
 
-        while queue:
-            level = []
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                level.append(node)
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
+            #  Scanner finds left-most neighbor, on same level, in right hand side
+            while scanner:
+                if scanner.left:
+                    scanner = scanner.left
+                    break
+                if scanner.right:
+                    scanner = scanner.right
+                    break
 
-            for i in range(len(level) - 1):
-                level[i].next = level[i + 1]
+                scanner = scanner.next
 
-        return root
+            if node.right:
+                node.right.next = scanner
+
+            if node.left:
+                node.left.next = node.right if node.right else scanner
+
+            helper(node.right)
+            helper(node.left)
+
+            return node
+
+        return helper(root)
