fe7510c24804f67f5061f6152d13ddd3959596bc
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-12-05 23:57:13 +0900

    use Character Code Manipulation and Shift Operation

--- leetcode/Daily_Question/2024_12_4.py
+++ leetcode/Daily_Question/2024_12_4.py
@@ -1,16 +1,15 @@
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
+        i = j = 0
+        m, n = len(str1), len(str2)
 
-        i, j = 0, 0  # iはstr1のポインタ、jはstr2のポインタ
-        n, m = len(str1), len(str2)
-
-        while i < n and j < m:
-            # 現在の文字が一致するか、1回増加させて一致する場合
-            if str1[i] == str2[j] or (
-                chr((ord(str1[i]) - ord("a") + 1) % 26 + ord("a")) == str2[j]
+        while i < m and j < n:
+            if (
+                str1[i] == str2[j]
+                or chr((ord(str1[i]) - ord("a") + 1) % 26 + ord("a")) == str2[j]
             ):
-                j += 1  # str2の次の文字に進む
-            i += 1  # str1の次の文字に進む
+                j += 1
 
-        # str2の全ての文字がチェックされた場合はTrue
-        return j == m
+            i += 1
+
+        return j == n
